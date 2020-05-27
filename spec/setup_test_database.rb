ba6e68eb80c3d7580b0e5e46aa9b3e49d3c13e1a
Joe <josephhull676@yahoo.co.uk>
2020-05-27 12:16:56 +0100

    Changed setup_test_database.rb so that users and spaces tables are truncated in makersbnb_test database

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -5,5 +5,6 @@ def setup_test_database
   p "Setting up test database..."
   connection = PG.connect(dbname: 'makersbnb_test')
 
-  connection.exec("TRUNCATE makersbnb;")
+  connection.exec("TRUNCATE users;")
+  connection.exec("TRUNCATE spaces;")
 end
