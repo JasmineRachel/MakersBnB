f47a8e694e590f3d02feaf48f48ee195f9ee82fb
Chan <chanpreetsingh974@gmail.com>
2020-05-27 11:27:22 +0100

    recommitted changes to space class

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ class MakersBnb < Sinatra::Base
     'Hello World'
   end
 
+  get '/add_space' do
+    erb :'add_space'
+  end
+
+  post '/add_space' do
+    erb :'add_space'
+    redirect :'index'
+  end
+  
+
+  post 
+
   run! if app_file == $0
 end
